4b5ac6e66f882d51110198a64bc12fad14b60f4e
creativeman497 <creativeman497@gmail.com>
2019-10-21 18:33:20 +1000

    client starter inputing

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import { Browser } from 'react-route-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>It works</h1>
+    </div>
+  );
+}
+
+export default App;
