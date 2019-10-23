f7bbbc41d6af2572466fc621203305e920824678
webmaster0307 <webmaster0307@gmail.com>
2019-10-23 10:59:41 -0400

    refactoring

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,42 @@
+const Event = require('../../models/event');
+const { transformEvent } = require('./merge');
+
+module.exports = {
+    events: async () => {
+        try {
+            const events = await Event.find();
+
+            return events.map(event => {
+                return transformEvent(event);
+            });
+        } catch (err) {
+            throw err;
+        }
+    },
+    createEvent: async args => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: '5daec2d434ccb20cbd409c16'
+        });
+
+        let createdEvent;
+        try {
+            const result = await event.save();
+
+            createdEvent = transformEvent(result);
+            const creator = await User.findById('5daec2d434ccb20cbd409c16');
+            if (!creator) {
+                throw new Error('User not found');
+            }
+            creator.createdEvents.push(event);
+            await creator.save();
+
+            return createdEvent;
+        } catch (err) {
+            throw err;
+        }
+    }
+};
